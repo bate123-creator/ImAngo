eb0479e905a67631801309c2b78ca5155139204a
s <bate6457@gmail.com>
2025-03-19 12:36:41 +0700

    Initial commit

---
+++ app/src/main/java/com/th/imgo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.th.imgo
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.th.imgo.ui.theme.ImAngoTheme
+import open_im_sdk.Open_im_sdk
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
